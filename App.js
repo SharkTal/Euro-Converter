c14105c6262aa7c37b051d8e445021ea9d127214
bgh252 <bgh252@gmail.com>
2021-09-02 23:49:09 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
 
 export default function App() {
+  const [amount, setAmount] = useState('');
+  const [rate, setRate] = useState();
+  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [countries, setCountries] = useState([]);
+  const [euro, setEuro] = useState('')
+  useEffect(() => {
+    const url = 'http://api.exchangeratesapi.io/v1/latest?access_key=eddb1f22cb5cd468457585b691802d84&format=1'
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setCountries(Object.keys(data.rates)))
+      .catch((error) => alert(error))
+  }, []
+  )
+
+  useEffect(() => {
+    const url = 'http://api.exchangeratesapi.io/v1/latest?access_key=eddb1f22cb5cd468457585b691802d84&format=1&symbols=' + selectedLanguage;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setRate(Object.values(data.rates)[0]))
+      .catch((error) => alert(error))
+  }, [selectedLanguage])
+
+
+
+  const convert = () => {
+    /*  console.log("amount"+amount, ">>>rate" + rate) */
+    const result = (amount / parseFloat(rate)).toFixed(2)
+    setEuro(result)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to use Euro Converter!</Text>
+      <Text>You can get {euro}  euros</Text>
+      <View style={styles.input_container}>
+        <TextInput
+          style={styles.input}
+          value={amount}
+          onChangeText={(text) => setAmount(parseFloat(text))}
+          placeholder='Enter money amount'
+          keyboardType='number-pad'
+        />
+        <Picker
+          style={styles.picker}
+          selectedValue={selectedLanguage}
+          onValueChange={(itemValue, itemIndex) =>
+            setSelectedLanguage(itemValue)
+          }>
+          {countries.map((item) => <Picker.Item label={item} value={item} />)}
+
+        </Picker>
+      </View>
+      <Button style={styles.button} onPress={convert} title='Convert' />
     </View>
   );
 }
@@ -18,4 +69,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input_container:{
+    flexDirection: 'row',
+    margin:100,
+    alignItems:'center',
+    justifyContent:'center',
+    paddingBottom:100
+  },
+  input: {
+    width: 100,
+    height: 50,
+    borderBottomColor: 'blue',
+    borderBottomWidth: 2,
+  },
+  picker: {
+    height: 10,
+    width: 100,
+
+  }
 });
